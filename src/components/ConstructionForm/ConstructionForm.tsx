a158c39d4dc514a2e2fae3ab683b82f52442521a
vapanteleev <panteleevvitja32@gmail.com>
2024-09-01 18:14:04 +0300

    линтер иди нахуй

--- src/components/ConstructionForm/ConstructionForm.tsx
+++ src/components/ConstructionForm/ConstructionForm.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { ConstructionContext } from '../../providers/ConstructionProvider/ConstructionProvider';
 import SurfaceForm from '../SurfaceForm/SurfaceForm';
 import { Surface } from '../../model/dataModel';
